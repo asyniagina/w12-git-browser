2ea459755ac32b6d744c057631aec18b05afe81f
Alina <alina.sinyagina@gmail.com>
2022-06-04 20:32:00 -0700

    add input

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 import Head from './head'
 // import wave from '../assets/images/wave.jpg'
 
 const Home = () => {
-  const [counter, setCounterNew] = useState(0)
+  const [value, setValue] = useState('')
+
+  const onChange = (e) => {
+    setValue(e.target.value)
+  }
+
 
   return (
     <div>
       <Head title="Dashboard" />
+      <input 
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
       <img alt="wave" src="images/wave.jpg" />
       <button type="button" onClick={() => setCounterNew(counter + 1)}>
         updateCounter
